6ac8355144798a02bc8ad4884091e6af8f1b4acd
myuuu <myuuu@live.fr>
2025-08-21 14:38:24 +0200

    init flask app with tests and lint

---
+++ test_app.py
@@ -0,0 +1,18 @@
+import pytest
+form app import app
+
+
+@pytest.fixture
+def client_app():
+    with app.test_client() as client:
+        yield client
+
+
+def test_health(client_app):
+    res = client_app.get("/health")
+    assert res.status_code == 200
+
+
+def test_hello(client_app):
+    res = client_app.get("/hello")
+    assert res.status_code == 200
