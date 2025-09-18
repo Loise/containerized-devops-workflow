6167059524fb450af8e582ef9d98ab86d1eb730f
myuuu <myuuu@live.fr>
2025-09-18 11:37:06 +0200

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -28,6 +28,29 @@ resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo apt-get update -y
+                sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+                # Ajout du dépôt Docker
+                curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+                sudo add-apt-repository \
+                "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+                sudo apt-get update -y
+                sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+                # Ajoute l'utilisateur ubuntu au groupe docker (optionnel)
+                sudo usermod -aG docker ubuntu
+
+                # Lancer le démon Docker
+                sudo systemctl enable docker
+                sudo systemctl start docker
+
+                # Tester l'installation Docker
+                sudo docker run hello-world
+              EOF
   tags = {
     Name = "learn-terraform"
   }
