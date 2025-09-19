65b355a67f09f19af2663f4dad2722b5d595e31a
myuuu <myuuu@live.fr>
2025-09-19 09:30:26 +0200

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -74,7 +74,7 @@ data "aws_security_group" "app_server_sg" {
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.app_server_sg.id]
+  vpc_security_group_ids = [data.aws_security_group.app_server_sg.id]
   user_data = <<-EOF
                 #!/bin/bash
                 sudo apt update
