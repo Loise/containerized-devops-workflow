c72d5a38015654742eb7bbbdd8eb693793260ad0
myuuu <myuuu@live.fr>
2025-09-21 15:26:16 +0200

    fix ip adress

--- infra/main.tf
+++ infra/main.tf
@@ -38,7 +38,7 @@ resource "aws_security_group" "app_server_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["88.169.169.190/32"]  # Remplacez par votre IP publique
+    cidr_blocks = ["0.0.0.0/0"]  # Remplacez par votre IP publique
   }
 
   ingress {
